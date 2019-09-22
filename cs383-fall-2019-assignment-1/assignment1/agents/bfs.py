01c0b7643c23766abff2466dc7670026bbc35305
Jack01999 <49932621+Jack01999@users.noreply.github.com>
2019-09-22 14:48:32 -0400

    Update bfs.py

--- cs383-fall-2019-assignment-1/assignment1/agents/bfs.py
+++ cs383-fall-2019-assignment-1/assignment1/agents/bfs.py
@@ -1,7 +1,26 @@
+import queue
+
 from .agent import Agent
 
 
 class BFS(Agent):
     def search(self, gridworld):
         # TODO
-        pass
+
+        frontier = queue.Queue()
+        frontier.put(gridworld.initial_state)
+        explored = []
+
+        while not frontier.empty():
+            node = frontier.get()
+            explored += node
+            for n in node:
+                if (n in frontier is False) and (n in explored is False):
+                    if n == gridworld.goal_state:
+                        return explored
+                    else:
+                        frontier += n
+
+        print(frontier.get())
+
+        return False
