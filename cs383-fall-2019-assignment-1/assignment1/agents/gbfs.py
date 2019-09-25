80cdf593741684f9d0f6a4e163287dcee3f84d90
Jack01999 <49932621+Jack01999@users.noreply.github.com>
2019-09-25 19:18:28 -0400

    Update gbfs.by

--- cs383-fall-2019-assignment-1/assignment1/agents/gbfs.py
+++ cs383-fall-2019-assignment-1/assignment1/agents/gbfs.py
@@ -1,7 +1,43 @@
+import heapq
+
 from .agent import Agent
 
 
 class GBFS(Agent):
+    def checkHeuristic(self, gridworld, node):
+        goal = gridworld.goal_state
+        x = abs(goal[0] - node[0])
+        y = abs(goal[1] - node[1])
+        heuristic = 1 * (x + y)
+        return heuristic
+
+    def is_in_frontier(self, frontier, n):
+        a = 0
+        while not a == len(frontier):
+            if frontier[a][1] == n:
+                return True
+            a += 1
+
+        return False
+
     def search(self, gridworld):
         # TODO
-        pass
+
+        explored = []
+        frontier = []  # initialize heap queue
+        node = gridworld.initial_state  # initial state
+        heapq.heappush(frontier, (self.checkHeuristic(gridworld, node), node))  # add the initial state into the queue.[ (heuristic, state) ]
+        foundPath = False
+
+        while not node == gridworld.goal_state:
+            node = heapq.heappop(frontier)
+            explored.append(node[1])
+            successors = gridworld.successors(node[1])
+
+
+            for n in successors:
+                print(n)
+                heuristic = self.checkHeuristic(gridworld, n)
+                if (not self.is_in_frontier(frontier, n)) and (n not in explored):
+
+
