a63bd2674abb2693fa6599ba826283ff1fb83c1b
Jack01999 <49932621+Jack01999@users.noreply.github.com>
2019-09-25 19:29:44 -0400

    Update gbfs.py

--- cs383-fall-2019-assignment-1/assignment1/agents/gbfs.py
+++ cs383-fall-2019-assignment-1/assignment1/agents/gbfs.py
@@ -28,16 +28,37 @@ class GBFS(Agent):
         node = gridworld.initial_state  # initial state
         heapq.heappush(frontier, (self.checkHeuristic(gridworld, node), node))  # add the initial state into the queue.[ (heuristic, state) ]
         foundPath = False
+        parent = {}
+        solution = []
+        cost = 0
+        nodes_expanded = 0
 
         while not node == gridworld.goal_state:
             node = heapq.heappop(frontier)
             explored.append(node[1])
             successors = gridworld.successors(node[1])
 
-
             for n in successors:
-                print(n)
                 heuristic = self.checkHeuristic(gridworld, n)
                 if (not self.is_in_frontier(frontier, n)) and (n not in explored):
+                    parent[n] = node[1]
+                    if n == gridworld.goal_state:
+                        nodes_expanded = len(explored)
+                        node = gridworld.goal_state
+                    else:
+                        heapq.heappush(frontier, (self.checkHeuristic(gridworld, n), n))
+
+        currentState = parent.get(gridworld.goal_state)
+        solution.insert(0, gridworld.goal_state)
+        while not currentState == gridworld.initial_state:
+            cost += gridworld.cost(currentState)
+            solution.insert(0, currentState)
+            currentState = parent.get(currentState)
+
+        cost += gridworld.cost(gridworld.goal_state)
+        solution.insert(0, gridworld.initial_state)
+
+        return solution, cost, nodes_expanded
+
 
 
