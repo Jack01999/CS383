a20be417060a43415fa660c8889f07d4e3e82c84
Jack01999 <49932621+Jack01999@users.noreply.github.com>
2019-09-26 02:41:36 -0400

    Update astar.py

--- cs383-fall-2019-assignment-1/assignment1/agents/astar.py
+++ cs383-fall-2019-assignment-1/assignment1/agents/astar.py
@@ -4,9 +4,9 @@ from .agent import Agent
 
 
 class AStar(Agent):
-    def checkHeuristic(self, end, node):
-        x = abs(end[0] - node[0])
-        y = abs(end[1] - node[1])
+    def checkHeuristic(self, start, end):
+        x = abs(end[0] - start[0])
+        y = abs(end[1] - start[1])
         heuristic = 1 * (x + y)
         return heuristic
 
@@ -19,35 +19,53 @@ class AStar(Agent):
 
         return False
 
+    def get_frontier_g(self, frontier, node):
+        a = 0
+        while not a == len(frontier):
+            if frontier[a][1] == node:
+                return frontier[a][0]
+            a += 1
+
     def search(self, gridworld):
         # TODO
-        explored = []
-        frontier = []  # initialize heap queue
-        node = gridworld.initial_state
-        heapq.heappush(frontier, (self.checkHeuristic(gridworld.goal_state, node), node))
+        frontier = []  # open list
+        explored = []  # closed list
+        heapq.heappush(frontier, (0, gridworld.initial_state))
         parent = {}
         solution = []
         cost = 0
         nodes_expanded = 0
 
         while not len(frontier) == 0:
-            node = heapq.heappop(frontier)  # node with the lowest heuristic
+            node = heapq.heappop(frontier)
+            successors = gridworld.successors(node[1])
+            node_g = 0
+            node_g += gridworld.cost(node[1])
+            nodes_expanded = len(explored)
             explored.append(node[1])
             if node == gridworld.goal_state:
-                break
-            successors = gridworld.successors(node[1])
+                continue
             for n in successors:
                 if n in explored:
                     continue
-                print(explored)
-                g = self.checkHeuristic(n, gridworld.initial_state) + self.checkHeuristic(n, node[1])
-                h = self.checkHeuristic(gridworld.goal_state, n)
-                f = g + h
-
+                n_g = node_g + self.checkHeuristic(n, node[1])
+                n_h = self.checkHeuristic(gridworld.goal_state, n)
+                n_f = n_g + n_h
                 if self.is_in_frontier(frontier, n):
-                    if g > self.checkHeuristic(node[1], gridworld.initial_state):
+                    if n_g > self.get_frontier_g(frontier, n):
                         continue
 
-                heapq.heappush(frontier, (self.checkHeuristic(gridworld.goal_state, n), n))
+                parent[n] = node[1]
+                heapq.heappush(frontier, (n_f, n))
+
+        currentState = parent.get(gridworld.goal_state)
+        solution.insert(0, gridworld.goal_state)
+        while not currentState == gridworld.initial_state:
+            cost += gridworld.cost(currentState)
+            solution.insert(0, currentState)
+            currentState = parent.get(currentState)
+
+        cost += gridworld.cost(gridworld.goal_state)
+        solution.insert(0, gridworld.initial_state)
 
         return solution, cost, nodes_expanded
