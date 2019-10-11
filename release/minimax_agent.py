4080c91c0c88e155b8e5e86819fd6de3b7d7d6e6
Jack01999 <49932621+Jack01999@users.noreply.github.com>
2019-10-10 20:27:15 -0400

    Update minimax_agent.py

--- release/minimax_agent.py
+++ release/minimax_agent.py
@@ -12,8 +12,55 @@ class MinimaxAgent(Agent):
         self.eval_fn = eval_fn
         self.prune = prune
 
+    def max_value(self, game, state):
+        if game.is_terminal(state):
+            return (None, game.utility(state))
+        Utility = float("-inf")
+        action = None
+        for a in game.get_actions(state):
+            updatedUtility = max(Utility, self.min_value(game, game.apply_action(state, a))[1])
+            if updatedUtility != Utility:
+                Utility = updatedUtility
+                action = a
+        return (action, Utility)
+
+    def min_value(self, game, state):
+        if game.is_terminal(state):
+            return (None, game.utility(state))
+        Utility = float("inf")
+        action = None
+        for a in game.get_actions(state):
+            updatedUtility = min(Utility, self.max_value(game, game.apply_action(state, a))[1])
+            if updatedUtility != Utility:
+                Utility = updatedUtility
+                action = a
+        return (action, Utility)
+
     def select_action(self, game, state):
+        if (state.min_to_play):
+            return self.min_value(game, state)[0]
+        return self.max_value(game, state)[0]
         """
         TODO: Implement the minimax algorithm
         """
-        pass
+
+        '''
+            State = (state, [board, min_pos, max_pos, min_to_play])
+            board = current board
+            min_pos = position of Min player
+            max_pos = position of Max player
+            min_to_play = Min player's turn
+            max_to_play = Max player's turn
+    
+            Action = (move, [move, delete])
+
+            set_init_state(self, state)
+            __moveable_to(self, cell, state)
+            get_actions(self, state)
+            apply_action(self, state, action)
+            utility(self, state)
+            is_terminal(self, state)
+            show(self, state)
+            play(self, min, max, verbose=True)
+            
+        '''
