b23f0f4ebd3fbf5ee167267b2a6cbf5338a88651
Jack01999 <49932621+Jack01999@users.noreply.github.com>
2019-10-10 20:27:35 -0400

    Update run.py

--- release/run.py
+++ release/run.py
@@ -1,5 +1,6 @@
 from game import Game, State
 from random_agent import RandomAgent
+from minimax_agent import MinimaxAgent
 import random
 
 
@@ -7,19 +8,27 @@ if __name__ == '__main__':
     g = Game(3)
 
     # Example of setting a custom start state for the game
-    # g.set_init_state(State(
-    #     board=[[0, 0, 1], [1, 1, 1], [1, 0, 1]],
+    #g.set_init_state(State(
+    #     board=[[1, 0, 1], [1, 1, 1], [1, 0, 1]],
     #     min_pos=(1, 0),
-    #     max_pos=(1, 1),
+    #     max_pos=(1, 2),
     #     min_to_play=True
-    # ))
+    #))
 
     # You can set the random seed to make your tests repeatable
     # random.seed(43110)
-    print("its working")
+
     # Create the agents to play in the game
-    min_player = RandomAgent()
+    min_player = MinimaxAgent()
     max_player = RandomAgent()
+    count = 0
+
+    for i in range(0, 1000):
+        final_utility, duration = g.play(min_player, max_player, verbose=False)
+        if final_utility == -1:
+            count = count + 1
+    print(count)
+
 
     # Run a complete game between the two players
-    g.play(min_player, max_player, verbose=True)
+    # g.play(min_player, max_player, verbose=True)
