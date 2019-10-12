fe05e833c8c1ed4d9e5093d2f1f43c3df08f6560
Jack01999 <49932621+Jack01999@users.noreply.github.com>
2019-10-11 20:31:47 -0400

    Update run.py

--- release/run.py
+++ release/run.py
@@ -1,11 +1,15 @@
 from game import Game, State
 from random_agent import RandomAgent
 from minimax_agent import MinimaxAgent
+from eval_fns import open_cells
+from eval_fns import my_eval_1
+from eval_fns import my_eval_2
+from eval_fns import my_best
 import random
 
 
 if __name__ == '__main__':
-    g = Game(3)
+    g = Game(5)
 
     # Example of setting a custom start state for the game
     #g.set_init_state(State(
@@ -19,15 +23,23 @@ if __name__ == '__main__':
     # random.seed(43110)
 
     # Create the agents to play in the game
-    min_player = MinimaxAgent()
-    max_player = RandomAgent()
-    count = 0
 
-    for i in range(0, 1000):
-        final_utility, duration = g.play(min_player, max_player, verbose=False)
+    min_player = MinimaxAgent(my_eval_1, 3)
+    max_player = MinimaxAgent(my_eval_2, 3)
+    min_count = 0
+    max_count = 0
+
+    for i in range(0, 1):
+        final_utility, duration = g.play(min_player, max_player, verbose=True)
         if final_utility == -1:
-            count = count + 1
-    print(count)
+            print("Min Player won")
+            min_count += 1
+        else:
+            print("Max player won")
+            max_count += 1
+
+    print(min_count)
+    print(max_count)
 
 
     # Run a complete game between the two players
