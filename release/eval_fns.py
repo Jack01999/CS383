cc0b19fbad75d293c0efb91b7de2626dd00747dc
Jack01999 <49932621+Jack01999@users.noreply.github.com>
2019-10-11 20:31:15 -0400

    Update eval_fns.py

--- release/eval_fns.py
+++ release/eval_fns.py
@@ -1,3 +1,7 @@
+from collections import namedtuple
+
+State = namedtuple('State', ['board', 'min_pos', 'max_pos', 'min_to_play'])
+
 """
 Evaluation functions for use with Minimax search.
 We have supplied you with one example function to get started.
@@ -34,14 +38,56 @@ def my_eval_1(game, state):
     Hint: Think about why open_cells is not a very good evaluation function!
     How might you improve it?
     """
-    pass
 
+    if game.is_terminal(state):
+        return game.utility(state)
+    else:
+        # the number of open cells next to the current player
+        open_cells = {cell for (cell, _) in game.get_actions(state)}
+        opponent_state = State(
+            board=state.board,
+            min_pos=state.min_pos,
+            max_pos=state.max_pos,
+            min_to_play=not state.min_to_play
+        )
+        # the number of open cells next to the opponent
+        opponent_open_cells = {cell for (cell, _) in game.get_actions(opponent_state)}
+        score = len(open_cells - opponent_open_cells) / 8
+        return -score if state.min_to_play else score
 
 def my_eval_2(game, state):
     """
     Write your second evaluation function here.
     """
-    pass
+    counter = 1
+    o_counter = 1
+    if game.is_terminal(state):
+        return game.utility(state)
+    else:
+        opponent_state = State(
+            board=state.board,
+            min_pos=state.min_pos,
+            max_pos=state.max_pos,
+            min_to_play=not state.min_to_play
+        )
+        player_counter = 0
+        opponent_counter = 0
+        for actions in game.get_actions(state):
+            counter += 1
+            newState = game.apply_action(state, actions)
+            open_cells = {cell for (cell, _) in game.get_actions(newState)}
+            player_counter += len(open_cells)
+
+        for actions in game.get_actions(opponent_state):
+            o_counter += 1
+            newState = game.apply_action(opponent_state, actions)
+            opponent_open_cells = {cell for (cell, _) in game.get_actions(opponent_state)}
+            opponent_counter += len(open_cells)
+
+        score = (player_counter / counter) - (opponent_counter / o_counter)
+        return -score if state.min_to_play else score
+
+
 
 
 def my_best(game, state):
@@ -49,4 +95,31 @@ def my_best(game, state):
     Call whichever one of your two functions you think is better
     and return the result here.
     """
-    pass
+
+    counter = 1
+    o_counter = 1
+    if game.is_terminal(state):
+        return game.utility(state)
+    else:
+        opponent_state = State(
+            board=state.board,
+            min_pos=state.min_pos,
+            max_pos=state.max_pos,
+            min_to_play=not state.min_to_play
+        )
+        player_counter = 0
+        opponent_counter = 0
+        for actions in game.get_actions(state):
+            counter += 1
+            newState = game.apply_action(state, actions)
+            open_cells = {cell for (cell, _) in game.get_actions(newState)}
+            player_counter += len(open_cells)
+
+        for actions in game.get_actions(opponent_state):
+            o_counter += 1
+            newState = game.apply_action(opponent_state, actions)
+            opponent_open_cells = {cell for (cell, _) in game.get_actions(opponent_state)}
+            opponent_counter += len(open_cells)
+
+        score = (player_counter / counter) - (opponent_counter / o_counter)
+        return -score if state.min_to_play else score
